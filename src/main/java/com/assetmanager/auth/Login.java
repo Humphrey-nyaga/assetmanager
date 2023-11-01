78d7551f4ccdafb264b6b05e4eb15b09b5991038
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-11-01 03:09:43 -0600

    Update To Use Dispatcher
    Added request dispatcher and send redirect

--- src/main/java/com/assetmanager/auth/Login.java
+++ src/main/java/com/assetmanager/auth/Login.java
@@ -1,5 +1,6 @@
 package com.assetmanager.auth;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -8,18 +9,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-@WebServlet(value = "/login",initParams = {
-        @WebInitParam(name="username",value = "admin"),
+
+@WebServlet(urlPatterns = "/login", initParams = {
+        @WebInitParam(name = "username", value = "admin"),
         @WebInitParam(name = "password", value = "admin")
 })
 public class Login extends HttpServlet {
+    public void doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.sendRedirect("./");
+    }
+
     public void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
         PrintWriter printWriter = servletResponse.getWriter();
 
         String username = servletRequest.getParameter("username");
         String password = servletRequest.getParameter("password");
-        if(username.equals(getInitParameter(username)) && password.equals(getInitParameter(password))){
-            printWriter.print("Welcome to Asset Manager");
+        if (username.equals(getInitParameter("username")) && password.equals(getInitParameter("password"))) {
+            RequestDispatcher requestDispatcher = servletRequest.getRequestDispatcher("/http-hello");
+            requestDispatcher.include(servletRequest, servletResponse);
+
+//            RequestDispatcher requestDispatcher2 = servletRequest.getRequestDispatcher("/home");
+//            requestDispatcher2.forward(servletRequest, servletResponse);
+
+            servletResponse.sendRedirect("./home.html");
         }
 
     }
