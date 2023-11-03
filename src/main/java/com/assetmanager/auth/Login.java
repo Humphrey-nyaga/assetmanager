eedcf0a351c80a65f6da26e99649cb5391517c73
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-11-03 01:47:53 -0600

    Update Login logic

--- src/main/java/com/assetmanager/auth/Login.java
+++ src/main/java/com/assetmanager/auth/Login.java
@@ -1,5 +1,7 @@
 package com.assetmanager.auth;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -8,8 +10,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 @WebServlet(urlPatterns = "/login", initParams = {
         @WebInitParam(name = "username", value = "admin"),
@@ -18,11 +22,18 @@ import java.io.PrintWriter;
 public class Login extends HttpServlet {
     public void doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
             throws ServletException, IOException {
+        HttpSession httpSession = servletRequest.getSession();
+        if (StringUtils.isNotBlank((String) httpSession.getAttribute("loggedInId"))){
+            servletResponse.sendRedirect("./home");
+        }
         servletResponse.sendRedirect("./");
     }
 
     public void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
             throws ServletException, IOException {
+        HttpSession httpSession = servletRequest.getSession(true);
+        httpSession.setAttribute("loggedInId", new Date().getTime()+"");
+
         PrintWriter printWriter = servletResponse.getWriter();
 
 //        String username = servletRequest.getParameter("username");
@@ -30,15 +41,15 @@ public class Login extends HttpServlet {
         ServletContext servletContext = getServletContext();
         String username = servletContext.getInitParameter("username");
         String password = servletContext.getInitParameter("password");
-        servletContext.setAttribute("username",username);
+        servletContext.setAttribute("username", username);
         if (username.equals(getInitParameter("username")) && password.equals(getInitParameter("password"))) {
             // RequestDispatcher requestDispatcher =
             // servletRequest.getRequestDispatcher("/http-hello");
             // requestDispatcher.include(servletRequest, servletResponse);
-             RequestDispatcher requestDispatcher =
-             servletRequest.getRequestDispatcher("/home");
-                         System.out.println("Request fetched");
-             requestDispatcher.include(servletRequest, servletResponse);
+            RequestDispatcher requestDispatcher =
+                    servletRequest.getRequestDispatcher("/home");
+            System.out.println("Request fetched");
+            requestDispatcher.include(servletRequest, servletResponse);
 
             //servletResponse.sendRedirect("/home");
         }
