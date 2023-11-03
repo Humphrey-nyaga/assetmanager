4364ce77a3034962672008bb68c9cefa1b89bc78
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-11-03 04:29:25 -0600

    Singleton Implementation
    Added a singleton class for database

--- src/main/java/com/assetmanager/app/database/Database.java
+++ src/main/java/com/assetmanager/app/database/Database.java
@@ -1,4 +1,29 @@
 package com.assetmanager.app.database;
 
-public class Database {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Database implements Serializable {
+    private Database databaseInstance;
+    private LocalDateTime dateTimeCreatedAt;
+
+    private Database() {
+    }
+
+    public Database getDatabaseInstance() {
+        if (databaseInstance == null) {
+            databaseInstance = new Database();
+            databaseInstance.setDateTimeCreatedAt(LocalDateTime.now());
+            System.out.println("Database created at: " + getDateTimeCreatedAt());
+        }
+        return databaseInstance;
+    }
+
+    private void setDateTimeCreatedAt(LocalDateTime dateTimeCreatedAt) {
+        this.dateTimeCreatedAt = dateTimeCreatedAt;
+    }
+
+    public LocalDateTime getDateTimeCreatedAt() {
+        return dateTimeCreatedAt;
+    }
 }
